4ddb8e331ceda3500be69390e43f52f7d8d1b3d7
StefanieSevens <stefaniesevens@aol.com>
2023-06-13 11:16:43 +0200

    commit13.06.23

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+let counter = 0
+
+
+
+const myCounter = function (req, res, next) {
+    npm FileReader  counter++ 
+    next()
+  }
+  
+  app.use(myCounter)
+
+
+
+app.get('/', (req, res) => 
+{
+  res.send('Hello World!' + counter)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
